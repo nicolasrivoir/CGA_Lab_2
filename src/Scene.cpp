7fa9767cc62d90aedf338e81a788e1e49eb2994a
Daniel Fernández Galli <daniel.fernandez.galli@fing.edu.uy>
2023-11-15 16:46:03 -0300

    Removed model testing rotation

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -22,5 +22,5 @@ void Scene::render()
 
 void Scene::update() {
 	//objects[0].translate(0.0f, 0.001f, 0.0f);
-	objects[0].rotate(1, Vector3(0, 0, 1));
+	//objects[0].rotate(1, Vector3(0, 0, 1));
 }
