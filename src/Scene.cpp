ad63fdd82eb1de1eb7e548565bd9c579ea44d0b8
Daniel Fernández Galli <daniel.fernandez.galli@fing.edu.uy>
2023-12-07 15:29:52 -0300

    Vehicle

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f777f1638b040b3dfaa9c4ec3b0e85aa278830249da8ef52d5765d0125cf09a5
+oid sha256:20a1cb1dc37e1a85cfc1f652e7026b98c4b190b0a04d28eab37235bcf921c5fc
 size 1747
